423aa85c929a6ba14a241c6fff52a48165a71afa
Yoni190 <yfisuhu@gmail.com>
2025-12-27 13:51:02 +0300

    Setup login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-       <p className='text-2xl font-bold'>Login</p>
+       <form action="#" className="w-3/4 max-w-sm mt-4 flex flex-col mx-auto">
+            <h1 className='text-2xl font-bold text-center'>Login</h1>
+           <input type="email" placeholder="Email" className="border p-2 rounded w-full mb-2" />
+           <input type="password" placeholder="Password" className="border p-2 rounded w-full mb-2" />
+           <button type="submit" className="bg-blue-500 text-white p-2 rounded">Login</button>
+       </form>
     </div>
   )
 }
