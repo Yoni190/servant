eae97ee92b2b3d6330b56672e7a65d9d8487ad31
Yoni190 <yfisuhu@gmail.com>
2025-12-28 10:37:39 +0300

    Make app name semibold

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
         <header className="bg-white shadow flex items-center p-6">
-            <h1 className="text-2xl font-bold">Servant</h1>
+            <h1 className="text-2xl font-semibold">Servant</h1>
         </header>
     </div>
   )
