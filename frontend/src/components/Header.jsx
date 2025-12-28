c82619a86438e83b10e8de9e954c5e4d8a51cca4
Yoni190 <yfisuhu@gmail.com>
2025-12-28 10:02:50 +0300

    Create Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <header className="bg-white shadow flex items-center p-6">
+            <h1 className="text-2xl font-bold">Servant</h1>
+        </header>
+    </div>
+  )
+}
+
+export default Header
