b9f73385b5833a1463334caf11afb137997ab8ca
Yoni190 <yfisuhu@gmail.com>
2025-12-28 10:22:58 +0300

    Show stat cards in home

---
+++ frontend/src/components/StatCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StatCard = ({ title, value }) => {
+  return (
+    <div className='bg-white p-4 rounded shadow'>
+      <p className='text-sm text-gray-500'>{title}</p>
+      <p className='text-2xl font-bold'>{value}</p>
+    </div>
+  )
+}
+
+export default StatCard
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../components/Header.jsx'
 import Sidebar from '../components/Sidebar.jsx'
+import StatCard from '../components/StatCard.jsx'
 
 const Home = () => {
   return (
@@ -8,7 +9,18 @@ const Home = () => {
       <Header />
       <div className="flex">
         <Sidebar />
-        <main className="flex-1 p-6">Main Content</main>
+        <main className="flex-1 p-6 bg-gray-50">
+
+          {/* Stat Cards */}
+          <div className="grid grid-cols-3 gap-3">
+            <StatCard title="Projects" value="5" />
+            <StatCard title="Services" value="18" />
+            <StatCard title="Team Members" value="12" />
+          </div>
+
+          {/* Charts */}
+          
+        </main>
       </div>
     </div>
   )
