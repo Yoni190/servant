4d68ef3d6fd8ac0c19db8ad2c4742025edc7c5bf
Yoni190 <yfisuhu@gmail.com>
2025-12-28 10:40:06 +0300

    Import app name from .env

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { APP_NAME } from '../config/appConfig'
+
 
 const Header = () => {
   return (
     <div>
         <header className="bg-white shadow flex items-center p-6">
-            <h1 className="text-2xl font-semibold">Servant</h1>
+            <h1 className="text-2xl font-semibold">{APP_NAME}</h1>
         </header>
     </div>
   )
