07133d1792ee819522983ffa95e9ac2ca27f4e3a
Yoni190 <yfisuhu@gmail.com>
2025-12-28 10:12:49 +0300

    Setup lucide icons

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import { FolderKanban, LogOut, Settings } from 'lucide-react'
 
 const Sidebar = () => {
   return (
     <div className="bg-gray-100 w-64 p-4 min-h-screen">
       <ul className='flex flex-col gap-5'>
-        <li className="mb-2">
+        <li className="mb-2 flex gap-2">
+          <FolderKanban className='text-gray-700'/>
           <a href="#" className="text-gray-700">Projects</a>
         </li>
-        <li className="mb-2">
+        <li className="mb-2 flex gap-2">
+          <Settings className='text-gray-700'/>
           <a href="#" className="text-gray-700">Settings</a>
         </li>
-        <li className="mb-2">
+        <li className="mb-2 flex gap-2">
+          <LogOut className='text-gray-700'/>
           <a href="#" className="text-gray-700">Log Out</a>
         </li>
       </ul>
