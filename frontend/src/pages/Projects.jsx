8358137c161fb57400ab4a80b47ffb705b1b7fd9
Yoni190 <yfisuhu@gmail.com>
2025-12-29 12:13:36 +0300

    Added Add New Project button

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -15,60 +15,69 @@ const Projects = () => {
       <div className="flex flex-1">
         <Sidebar />
         <main className="flex-1 p-6 bg-gray-50 space-y-6">
-            <h1 className='text-2xl font-semibold'>Projects</h1>
-            <div className="bg-white rounded shadow p-4 ">
-                <h2 className='text-lg font-semibold'>Filter</h2>
-                <form action="#" className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-4'>
-                    <input 
-                        className='border border-gray-300 rounded px-3 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
-                        type="text"
-                        name="title"
-                        id="title"
-                        placeholder='Project Title'/>
-                    <input 
-                        className='border border-gray-300 rounded px-3 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
-                        type="number"
-                        name="services"
-                        id="services"
-                        placeholder='Number of Services'/>
-                    <input 
-                        className='border border-gray-300 rounded px-3 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
-                        type="number"
-                        name="members"
-                        id="members"
-                        placeholder='Number of Team Members'/>
-                    <button 
-                        type="submit"
-                        className='bg-blue-500 text-white px-4 py-2 rounded mt-2 hover:bg-blue-600 w-32'>
-                        Apply Filters
-                    </button>
-                </form>
+
+            {/* Title */}
+            <div className='flex justify-between'>
+                <h1 className='text-2xl font-semibold'>Projects</h1>
+                <button className='bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600'>Add New Project</button>
             </div>
-            <table className='w-full bg-white rounded shadow overflow-hidden'>
-                <thead className='bg-gray-100 text-left'>
-                    <tr>
-                        <th className='p-3'>ID</th>
-                        <th className='p-3'>Title</th>
-                        <th className='p-3'>Services</th>
-                        <th className='p-3'>Team Members</th>
-                        <th className='p-3'>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {projects.map((project) => (
-                        <tr key={project.id} className='border-t hover:bg-gray-50'>
-                            <td className='p-3'>{project.id}</td>
-                            <td className='p-3 font-medium'>{project.title}</td>
-                            <td className='p-3'>{project.services}</td>
-                            <td className='p-3'>{project.members}</td>
-                            <td className='p-3 space-x-3'>
-                                <button className="text-blue-500 hover:underline hover:cursor-pointer">Edit</button>
-                                <button className="text-red-500 hover:underline hover:cursor-pointer">Delete</button>
-                            </td>
+
+            {/* Body */}
+            <div className='space-y-6'>
+                <div className="bg-white rounded shadow p-4 ">
+                    <h2 className='text-lg font-semibold'>Filter</h2>
+                    <form action="#" className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-4'>
+                        <input
+                            className='border border-gray-300 rounded px-3 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                            type="text"
+                            name="title"
+                            id="title"
+                            placeholder='Project Title'/>
+                        <input
+                            className='border border-gray-300 rounded px-3 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                            type="number"
+                            name="services"
+                            id="services"
+                            placeholder='Number of Services'/>
+                        <input
+                            className='border border-gray-300 rounded px-3 py-2 mt-2 focus:outline-none focus:ring-2 focus:ring-blue-500'
+                            type="number"
+                            name="members"
+                            id="members"
+                            placeholder='Number of Team Members'/>
+                        <button
+                            type="submit"
+                            className='bg-blue-500 text-white px-4 py-2 rounded mt-2 hover:bg-blue-600 w-32'>
+                            Apply Filters
+                        </button>
+                    </form>
+                </div>
+                <table className='w-full bg-white rounded shadow overflow-hidden'>
+                    <thead className='bg-gray-100 text-left'>
+                        <tr>
+                            <th className='p-3'>ID</th>
+                            <th className='p-3'>Title</th>
+                            <th className='p-3'>Services</th>
+                            <th className='p-3'>Team Members</th>
+                            <th className='p-3'>Actions</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {projects.map((project) => (
+                            <tr key={project.id} className='border-t hover:bg-gray-50'>
+                                <td className='p-3'>{project.id}</td>
+                                <td className='p-3 font-medium'>{project.title}</td>
+                                <td className='p-3'>{project.services}</td>
+                                <td className='p-3'>{project.members}</td>
+                                <td className='p-3 space-x-3'>
+                                    <button className="text-blue-500 hover:underline hover:cursor-pointer">Edit</button>
+                                    <button className="text-red-500 hover:underline hover:cursor-pointer">Delete</button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </main>
       </div>
     </div>
