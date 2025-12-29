f7981ba55e7145cf2b68afaca7c3b48c8c83bf10
Yoni190 <yfisuhu@gmail.com>
2025-12-29 12:19:29 +0300

    Make icons clicable as well

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,21 +6,29 @@ const Sidebar = () => {
   return (
     <div className="bg-gray-100 w-64 p-4">
       <ul className='flex flex-col gap-5'>
-        <li className="mb-2 flex gap-2">
-          <Home className='text-gray-700'/>
-          <Link to="/home" className="text-gray-700">Home</Link>
+        <li className="mb-2">
+          <Link to="/home" className="text-gray-700 flex gap-2">
+            <Home className='text-gray-700'/>
+            Home
+          </Link>
         </li>
-        <li className="mb-2 flex gap-2">
-          <FolderKanban className='text-gray-700'/>
-          <Link to="/projects" className="text-gray-700">Projects</Link>
+        <li className="mb-2">
+          <Link to="/projects" className="text-gray-700 flex gap-2">
+            <FolderKanban className='text-gray-700'/>
+            Projects
+          </Link>
         </li>
-        <li className="mb-2 flex gap-2">
-          <Logs className='text-gray-700'/>
-          <a href="#" className="text-gray-700">Logs</a>
+        <li className="mb-2">
+          <Link to="/logs" className="text-gray-700 flex gap-2">
+            <Logs className='text-gray-700'/>
+            Logs
+          </Link>
         </li>
-        <li className="mb-2 flex gap-2">
-          <Settings className='text-gray-700'/>
-          <a href="#" className="text-gray-700">Settings</a>
+        <li className="mb-2">
+          <Link to="/settings" className="text-gray-700 flex gap-2">
+            <Settings className='text-gray-700'/>
+            Settings
+          </Link>
         </li>
         <li className="mb-2 flex gap-2">
           <LogOut className='text-gray-700'/>
