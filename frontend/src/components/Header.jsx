f03fce8c902e7985483a35408e71c389dd65f068
Yoni190 <yfisuhu@gmail.com>
2025-12-30 23:03:51 +0300

    Translate header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { APP_NAME } from '../config/appConfig'
+import { useTranslation } from 'react-i18next'
 
 
 const Header = () => {
+
+  const { t } = useTranslation();
+
   return (
     <div>
         <header className="bg-white shadow flex items-center p-6 dark:bg-gray-900">
-            <h1 className="text-2xl font-semibold dark:text-white">{APP_NAME}</h1>
+            <h1 className="text-2xl font-semibold dark:text-white">{t('appName')}</h1>
         </header>
     </div>
   )
