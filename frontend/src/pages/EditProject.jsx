3342d64bb0715e4c9f9e44f577000cef5c1658a6
Yoni190 <yfisuhu@gmail.com>
2025-12-30 23:45:40 +0300

    Translate Edit Project page

--- frontend/src/pages/EditProject.jsx
+++ frontend/src/pages/EditProject.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
 import ProjectForm from '../components/ProjectForm'
+import { useTranslation } from 'react-i18next'
+
 
 
 const EditProject = () => {
 
     const services_data = ["Service A", "Service B", "Service C"]
+
+    const { t } = useTranslation();
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex flex-1">
         <Sidebar />
         <main className="flex-1 p-6 bg-gray-50 space-y-6 dark:bg-gray-700">
-            <h1 className='text-2xl font-semibold dark:text-white'>Edit Project</h1>
+            <h1 className='text-2xl font-semibold dark:text-white'>{t('editProject')}</h1>
             <ProjectForm 
                 title_pass="Alpha"
                 description_pass="This is the Alpha project."
