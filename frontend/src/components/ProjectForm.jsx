23eabe0a161a811e9ba72b2892b2d2174cb23547
Yoni190 <yfisuhu@gmail.com>
2026-01-02 11:59:34 +0300

    Add form for service info

--- frontend/src/components/ProjectForm.jsx
+++ frontend/src/components/ProjectForm.jsx
@@ -94,127 +94,176 @@ const ProjectForm = ({ title_pass, description_pass, services_pass, members_pass
     }
 
   return (
-        <form className="w-full max-w-lg bg-white rounded-2xl shadow-lg p-8 space-y-5 dark:bg-gray-800">
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white" htmlFor="title">
-                    {t('projectTitle')}
-                </label>
-                <input
-                    type="text"
-                    id="title"
-                    className="w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition dark:bg-gray-700 dark:text-white dark:border-gray-600"
-                    placeholder={t('enterProjectTitle')}
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                {errors.find(error => error.field === 'title') && (
-                    <p className="text-red-500 text-xs mt-2">
-                        {errors.find(error => error.field === 'title').message}
-                    </p>
-                )}
-            </div>
-
-            <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white" htmlFor="description">
-                    {t('projectDescription')}
-                </label>
-                <textarea
-                    id="description"
-                    className="w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition dark:bg-gray-700 dark:text-white dark:border-gray-600"
-                    placeholder={t('enterProjectDescription')}
-                    rows="4"
-                    value={description}
-                    onChange={(e) => setDescription(e.target.value)}
-                ></textarea>
-                {errors.find(error => error.field === 'description') && (
-                    <p className="text-red-500 text-xs mt-2">
-                        {errors.find(error => error.field === 'description').message}
-                    </p>
-                )}
-            </div>
-
-            <div className="mb-4">
-                <label htmlFor="services" className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">{t('projectServices')}</label>
-                <div className='flex'>
+        <div className="dark:bg-gray-800 flex gap-5">
+            <form className='bg-white rounded-2xl shadow-lg p-8 space-y-5 w-full max-w-lg'>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white" htmlFor="title">
+                        {t('projectTitle')}
+                    </label>
                     <input
                         type="text"
-                        id="services"
+                        id="title"
                         className="w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition dark:bg-gray-700 dark:text-white dark:border-gray-600"
-                        placeholder={t('enterProjectServices')}
-                        value={servicesElement}
-                        onChange={(e) => setServicesElement(e.target.value)}
-                        onKeyDown={(e) => {
-                            if(e.key === 'Enter') {
-                                e.preventDefault()
-                                handleServicesAddition()
+                        placeholder={t('enterProjectTitle')}
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                    {errors.find(error => error.field === 'title') && (
+                        <p className="text-red-500 text-xs mt-2">
+                            {errors.find(error => error.field === 'title').message}
+                        </p>
+                    )}
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2 dark:text-white" htmlFor="description">
+                        {t('projectDescription')}
+                    </label>
+                    <textarea
+                        id="description"
+                        className="w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition dark:bg-gray-700 dark:text-white dark:border-gray-600"
+                        placeholder={t('enterProjectDescription')}
+                        rows="4"
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                    ></textarea>
+                    {errors.find(error => error.field === 'description') && (
+                        <p className="text-red-500 text-xs mt-2">
+                            {errors.find(error => error.field === 'description').message}
+                        </p>
+                    )}
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="services" className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">{t('projectServices')}</label>
+                    <div className='flex'>
+                        <input
+                            type="text"
+                            id="services"
+                            className="w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition dark:bg-gray-700 dark:text-white dark:border-gray-600"
+                            placeholder={t('enterProjectServices')}
+                            value={servicesElement}
+                            onChange={(e) => setServicesElement(e.target.value)}
+                            onKeyDown={(e) => {
+                                if(e.key === 'Enter') {
+                                    e.preventDefault()
+                                    handleServicesAddition()
+                                }
                             }
                         }
-                    }
-                    />
-                    <button 
-                    className='border px-4 rounded bg-blue-500 text-white hover:bg-blue-600 hover:cursor-pointer'
-                    type='button'
-                    onClick={handleServicesAddition}
-                    >
-                        +
-                    </button>
-                </div>
-                <div className='grid grid-cols-4 gap-3 mt-2'>
-                    {services.map((service, index) => (
-                        
-                            <div 
-                                key={index}
-                                className='bg-gray-300 p-2 border flex justify-between'
-                                >
-                                <p>
-                                    {service}
-                                </p>
-
-                                <button
-                                    className='cursor-pointer'
-                                    onClick={() => handleServiceDeletion(index)}
-                                    type='button'
-                                    >
-                                        x
-                                    </button>
-                                
-                            </div>
-                        
-                    ))}
-                </div>
-                {errors.find(error => error.field === 'services') && (
-                    <p className="text-red-500 text-xs mt-2">
-                        {errors.find(error => error.field === 'services').message}
-                    </p>
-                )}
-            </div>
-
-            <div className="mb-4">
-                <label htmlFor="members" className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">{t('projectMembers')}</label>
-                <input
-                    type="text"
-                    id="members"
-                    className="w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition dark:bg-gray-700 dark:text-white dark:border-gray-600"
-                    placeholder={t('enterProjectMembers')}
-                    value={members}
-                    onChange={(e) => setMembers(e.target.value)}
-                />
-                {errors.find(error => error.field === 'members') && (
-                    <p className="text-red-500 text-xs mt-2">
-                        {errors.find(error => error.field === 'members').message}
-                    </p>
-                )}
-            </div>
-
-            <button
-                type="submit"
-                className="w-full bg-green-600 hover:bg-green-700 text-white py-2.5 rounded-lg font-medium transition dark:bg-green-500 dark:hover:bg-green-600"
-                onClick={handleSubmit}
-            >
-                {t('saveProject')}
-            </button>
+                        />
+                        <button
+                        className='border px-4 rounded bg-blue-500 text-white hover:bg-blue-600 hover:cursor-pointer'
+                        type='button'
+                        onClick={handleServicesAddition}
+                        >
+                            +
+                        </button>
+                    </div>
+                    <div className='grid grid-cols-4 gap-3 mt-2'>
+                        {services.map((service, index) => (
             
-        </form>
+                                <div
+                                    key={index}
+                                    className='bg-gray-300 p-2 border flex justify-between'
+                                    >
+                                    <p>
+                                        {service}
+                                    </p>
+                                    <button
+                                        className='cursor-pointer'
+                                        onClick={() => handleServiceDeletion(index)}
+                                        type='button'
+                                        >
+                                            x
+                                        </button>
+            
+                                </div>
+            
+                        ))}
+                    </div>
+                    {errors.find(error => error.field === 'services') && (
+                        <p className="text-red-500 text-xs mt-2">
+                            {errors.find(error => error.field === 'services').message}
+                        </p>
+                    )}
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="members" className="block text-gray-700 text-sm font-bold mb-2 dark:text-white">{t('projectMembers')}</label>
+                    <input
+                        type="text"
+                        id="members"
+                        className="w-full rounded-lg border border-gray-300 px-4 py-2.5 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition dark:bg-gray-700 dark:text-white dark:border-gray-600"
+                        placeholder={t('enterProjectMembers')}
+                        value={members}
+                        onChange={(e) => setMembers(e.target.value)}
+                    />
+                    {errors.find(error => error.field === 'members') && (
+                        <p className="text-red-500 text-xs mt-2">
+                            {errors.find(error => error.field === 'members').message}
+                        </p>
+                    )}
+                </div>
+                <button
+                    type="submit"
+                    className="w-full bg-green-600 hover:bg-green-700 text-white py-2.5 rounded-lg font-medium transition dark:bg-green-500 dark:hover:bg-green-600"
+                    onClick={handleSubmit}
+                >
+                    {t('saveProject')}
+                </button>
+            
+            </form>
+
+            <div className='w-full max-w-lg rounded bg-white shadow-lg p-4'>
+                    {/* Tabs */}
+                    <div>
+                        <button
+                        className='border rounded bg-blue-500 text-white p-2 hover:bg-blue-600 hover:cursor-pointer'
+                        >
+                            Chapa
+                        </button>
+
+                        <button
+                        className='border rounded bg-gray-400 text-white p-2 hover:bg-gray-600 hover:cursor-pointer'
+                        >
+                            Taqnayat
+                        </button>
+                    </div>
+
+                    {/* Service URL */}
+                    <div className='flex flex-col mt-2'>
+                        <label htmlFor="link">Service URL</label>
+                        <input
+                        type="text"
+                        name="link"
+                        placeholder='Enter service url'
+                        className='border rounded p-2'
+                        id="link" />
+                    </div>
+
+                    {/* Service Email */}
+                    <div className='flex flex-col mt-2'>
+                        <label htmlFor="email">Service Email</label>
+                        <input
+                        type="email"
+                        name="email"
+                        placeholder='Enter email used for the service'
+                        className='border rounded p-2'
+                        id="email" />
+                    </div>
+
+                    {/* Service Password */}
+                    <div className='flex flex-col mt-2'>
+                        <label htmlFor="password">Service Password</label>
+                        <input
+                        type="password"
+                        name="password"
+                        placeholder='Enter password used for the service'
+                        className='border rounded p-2'
+                        id="password" />
+                    </div>
+
+                    <button className='border rounded bg-blue-500 text-white p-2 mt-2 w-full hover:bg-blue-600 hover:cursor-pointer'>Save</button>
+            </div>
+        </div>
   )
 }
 
