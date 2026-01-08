d89a2aba887f067c1f9fb4bc69e17ae4f14239d1
Yoni190 <yfisuhu@gmail.com>
2026-01-08 09:13:42 +0300

    Store passed id in a variable

--- frontend/src/pages/ProjectDescription.jsx
+++ frontend/src/pages/ProjectDescription.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
+import { useParams } from 'react-router-dom'
 
 
 const ProjectDescription = () => {
+
+    const { id } = useParams()
+    
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
