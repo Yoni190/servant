c78a6e58d1b568dae1cb6d2a68c48037c978339f
Yoni190 <yfisuhu@gmail.com>
2026-01-08 09:12:56 +0300

    Add header and sidebar to project description page

--- frontend/src/pages/ProjectDescription.jsx
+++ frontend/src/pages/ProjectDescription.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+
 
 const ProjectDescription = () => {
   return (
-    <div>ProjectDescription</div>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <div className="flex flex-1">
+        <Sidebar />
+        <main className="flex-1 p-6 bg-gray-50 space-y-6 dark:bg-gray-700">
+            <h1 className='text-2xl font-semibold dark:text-white'>Project Description</h1>
+        </main>
+      </div>
+    </div>
   )
 }
 
