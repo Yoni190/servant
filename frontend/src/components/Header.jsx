5efeff28eecd1d2a9b5a1ddfdfa4d1d961446314
Yoni190 <yfisuhu@gmail.com>
2026-01-11 12:05:11 +0300

    Show hamburger menu on smaller screens

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { APP_NAME } from '../config/appConfig'
 import { useTranslation } from 'react-i18next'
+import { Menu } from 'lucide-react'
+
 
 
 const Header = () => {
@@ -9,8 +11,9 @@ const Header = () => {
 
   return (
     <div>
-        <header className="bg-white shadow flex items-center p-6 dark:bg-gray-900">
+        <header className="bg-white shadow flex items-center p-6 dark:bg-gray-900 justify-between">
             <h1 className="text-2xl font-semibold dark:text-white">{t('appName')}</h1>
+            <Menu className='md:hidden'/>
         </header>
     </div>
   )
